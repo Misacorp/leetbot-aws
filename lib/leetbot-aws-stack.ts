8230f3ac5e4ccd20816303b454f387cbf2bd0ba3
Misa Jokisalo <misa.jokisalo@knowit.fi>
2023-05-27 12:00:15 +0300

    Added preliminary Fargate service creation

--- lib/leetbot-aws-stack.ts
+++ lib/leetbot-aws-stack.ts
@@ -3,26 +3,34 @@ import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
+import createFargateServices from "../bin/createFargateServices";
+
+const createTestStack = (stack: Stack): void => {
+  const queue = new sqs.Queue(stack, "LeetbotAwsQueue", {
+    visibilityTimeout: Duration.seconds(300),
+  });
+
+  const topic = new sns.Topic(stack, "LeetbotAwsTopic");
+
+  topic.addSubscription(new subs.SqsSubscription(queue));
+
+  // eslint-disable-next-line no-new
+  new CfnOutput(stack, "SQSQueueUrl", {
+    description: "SQS queue URL",
+    value: queue.queueUrl,
+  });
+};
 
 /**
- * Main Cloudformation stack.
+ * Main Cloudformation stack
  */
 export class LeetbotAwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "LeetbotAwsQueue", {
-      visibilityTimeout: Duration.seconds(300),
-    });
+    // Test stack temporarily used to see if stuff happens
+    createTestStack(this);
 
-    const topic = new sns.Topic(this, "LeetbotAwsTopic");
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-
-    // eslint-disable-next-line no-new
-    new CfnOutput(this, "SQSQueueUrl", {
-      description: "SQS queue URL",
-      value: queue.queueUrl,
-    });
+    createFargateServices(this);
   }
 }
