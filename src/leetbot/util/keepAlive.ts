2403599ff5e3be5c4e088fc081109f81c0427c2d
Misacorp <misa.jokisalo@gmail.com>
2023-10-12 23:31:59 +0300

    Add leet messages to SQS queue. Add timeout override for watcher Lambda to facilitate manual testing.

--- src/leetbot/util/keepAlive.ts
+++ src/leetbot/util/keepAlive.ts
@@ -1,23 +1,27 @@
-import { Context } from "aws-lambda";
+import type { Context, ScheduledEvent } from "aws-lambda";
 
-// Timeout interval to use when keeping the function alive
-const INTERVAL_MS = 1000;
 // How long to leave for the function to log out of Discord and exit
-const EXIT_THRESHOLD = 2000;
+const EXIT_THRESHOLD = 4000;
 
 /**
  * Keeps a Lambda function alive by maintaining entities in the Javascript call stack and event queue.
+ * @param event   Lambda event
  * @param context Lambda execution context
  */
-const keepAlive = async (context: Context) => {
-  const timeRemaining = context.getRemainingTimeInMillis();
+const keepAlive = async (
+  event: ScheduledEvent | { timeoutOverrideMs?: number },
+  context: Context,
+) => {
+  // Allow overriding the time this function runs for.
+  // Useful when testing Lambdas with manual invocation.
+  const timeoutOverrideMs =
+    "timeoutOverrideMs" in event ? event.timeoutOverrideMs : undefined;
 
-  if (timeRemaining > INTERVAL_MS + EXIT_THRESHOLD) {
-    await new Promise((resolve) => {
-      setTimeout(resolve, INTERVAL_MS);
-    });
-    await keepAlive(context);
-  }
+  const timeRemaining = timeoutOverrideMs ?? context.getRemainingTimeInMillis();
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, timeRemaining - EXIT_THRESHOLD);
+  });
 };
 
 export default keepAlive;
