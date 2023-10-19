971db8122c88fecf1f9951e126b9ba6ea1adbbff
Misacorp <misa.jokisalo@gmail.com>
2023-10-19 20:00:38 +0300

    Fix dateTime tests and epoch handling in date creation

--- src/leetbot/util/dateTime.ts
+++ src/leetbot/util/dateTime.ts
@@ -18,22 +18,22 @@ export const toEpoch = (date: Date) => Math.floor(date.getTime() / 1000);
 
 /**
  * Determines if a message was created within the time window of [13:37, 13:38[
- * @param timestamp Timestamp as a unix epoch
- * @param timezone  Target timezone
+ * @param epoch    Timestamp as a unix epoch
+ * @param timezone Target timezone
  */
-export const isLeet = (timestamp: number, timezone = DEFAULT_TIMEZONE) => {
-  const createdAt: Date = utcToZonedTime(new Date(timestamp), timezone);
+export const isLeet = (epoch: number, timezone = DEFAULT_TIMEZONE) => {
+  const createdAt: Date = utcToZonedTime(new Date(epoch * 1000), timezone);
 
   return createdAt.getHours() === 13 && createdAt.getMinutes() === 37;
 };
 
 /**
  * Determines if a message was created within the time window of [13:38, 13:39[
- * @param timestamp Timestamp as a unix epoch
- * @param timezone  Target timezone
+ * @param epoch    Timestamp as a unix epoch
+ * @param timezone Target timezone
  */
-export const isLeeb = (timestamp: number, timezone = DEFAULT_TIMEZONE) => {
-  const createdAt: Date = utcToZonedTime(new Date(timestamp), timezone);
+export const isLeeb = (epoch: number, timezone = DEFAULT_TIMEZONE) => {
+  const createdAt: Date = utcToZonedTime(new Date(epoch * 1000), timezone);
 
   return createdAt.getHours() === 13 && createdAt.getMinutes() === 38;
 };
