3d5e81632d240a9af9327d4297cec194ad533937
Misacorp <misa.jokisalo@gmail.com>
2023-10-20 22:03:51 +0300

    Add message mocks, fix epoch tests, resolve Jest module extension loading order issue

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  resolver: "ts-jest-resolver",
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   roots: ["<rootDir>/test", "<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  moduleFileExtensions: ["ts", "js", "mjs", "cjs", "json", "node"],
   transform: {
     "^.+\\.(ts)x?$": "ts-jest",
   },
   transformIgnorePatterns: ["^.+\\.js$"],
   moduleNameMapper: {
+    "^/opt/nodejs/discord": "<rootDir>/src/layers/discord/nodejs/discord",
     "^/opt/nodejs/date-fns": "<rootDir>/src/layers/date-fns/nodejs/date-fns",
   },
 };
--- src/leetbot/util/dateTime.ts
+++ src/leetbot/util/dateTime.ts
@@ -11,18 +11,12 @@ export { utcToZonedTime };
 const DEFAULT_TIMEZONE = "Europe/Helsinki";
 
 /**
- * Converts a time to a unix epoch
- * @param date Date
- */
-export const toEpoch = (date: Date) => Math.floor(date.getTime() / 1000);
-
-/**
  * Determines if a message was created within the time window of [13:37, 13:38[
  * @param epoch    Timestamp as a unix epoch
  * @param timezone Target timezone
  */
 export const isLeet = (epoch: number, timezone = DEFAULT_TIMEZONE) => {
-  const createdAt: Date = utcToZonedTime(new Date(epoch * 1000), timezone);
+  const createdAt: Date = utcToZonedTime(new Date(epoch), timezone);
 
   return createdAt.getHours() === 13 && createdAt.getMinutes() === 37;
 };
@@ -33,7 +27,7 @@ export const isLeet = (epoch: number, timezone = DEFAULT_TIMEZONE) => {
  * @param timezone Target timezone
  */
 export const isLeeb = (epoch: number, timezone = DEFAULT_TIMEZONE) => {
-  const createdAt: Date = utcToZonedTime(new Date(epoch * 1000), timezone);
+  const createdAt: Date = utcToZonedTime(new Date(epoch), timezone);
 
   return createdAt.getHours() === 13 && createdAt.getMinutes() === 38;
 };
