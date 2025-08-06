40d456e55cbf0345144cc6ba4c9f10962bd86a1a
Misacorp <misa.jokisalo@gmail.com>
2025-08-06 10:25:46 +0300

    Remove LEET (etc.) message handling from Discord watcher and forward them directly to SNS instead (WIP)

--- src/leetbot/discordWatcher.ts
+++ src/leetbot/discordWatcher.ts
@@ -2,26 +2,20 @@ import type { Context, ScheduledEvent } from "aws-lambda";
 import { Client, Events, IntentsBitField } from "/opt/nodejs/discord";
 import keepAlive from "./util/lambda";
 import { getSecret } from "./util/secrets";
-import { leetHandler } from "./messageHandlers/leetHandler";
-import { leebHandler } from "./messageHandlers/leebHandler";
-import { failedLeetHandler } from "./messageHandlers/failedLeetHandler";
-import { unrelatedHandler } from "./messageHandlers/unrelatedHandler";
-import type { MessageHandlerProps, TestEvent } from "../types";
+import type { TestEvent } from "../types";
+import { publishToDiscordOutTopic } from "./messageHandlers/publishToDiscordOutTopic";
 
-const messageHandlers: ((props: MessageHandlerProps) => Promise<void>)[] = [
-  leetHandler,
-  leebHandler,
-  failedLeetHandler,
-  unrelatedHandler,
-];
-
+/**
+ * Discord watcher.
+ * Listens to messages on a Discord server and forwards them for further processing.
+ * @param event   Event that started the watcher.
+ * @param context Lambda context
+ */
 export const handler = async (
   event: ScheduledEvent | TestEvent,
   context: Context,
 ) => {
-  // Get Discord bot token
   const token = await getSecret(process.env.TOKEN_SECRET_ID!);
-
   const client = new Client({
     intents: new IntentsBitField()
       .add(IntentsBitField.Flags.Guilds)
@@ -30,11 +24,43 @@ export const handler = async (
     closeTimeout: 300,
   });
 
-  client.once(Events.ClientReady, async (c) => {
-    console.info(`Logged in to Discord as ${c.user.tag}.`);
+  initEventHandlers(client, event);
 
-    const guild = await client.guilds.fetch("215386000132669440");
-    console.info("Guild name:", guild.name);
+  // Start the bot lifecycle
+  console.info("Logging in to Discord…");
+  await client.login(token);
+
+  console.info("Keeping the bot alive…");
+  await keepAlive(event, context);
+
+  console.info("Logging out of Discord…");
+  await client.destroy();
+
+  console.info("Exiting Lambda…");
+};
+
+/**
+ * Client ready handler
+ * @param client Discord client
+ */
+const onClientReady = async (client: Client<true>) => {
+  console.info(`Logged in to Discord as ${client.user.tag}.`);
+
+  const guild = await client.guilds.fetch("215386000132669440");
+  console.info("Guild name:", guild.name);
+};
+
+/**
+ * Initialize event handlers for the Discord watcher
+ * @param client Discord client
+ * @param event  Event that started the Discord watcher
+ */
+const initEventHandlers = (
+  client: Client,
+  event: ScheduledEvent | TestEvent,
+) => {
+  client.once(Events.ClientReady, (resolvedClient) => {
+    void onClientReady(resolvedClient);
   });
 
   client.once(Events.Error, (error) => {
@@ -42,24 +68,8 @@ export const handler = async (
     console.error(error);
   });
 
-  client.on(Events.MessageCreate, async (message) => {
-    // Run the message through all message handlers simultaneously
-    await Promise.all(
-      messageHandlers.map((messageHandler) =>
-        messageHandler({ message, event }),
-      ),
-    );
+  client.on(Events.MessageCreate, (message) => {
+    // Blindly pass all messages to SNS for further processing
+    void publishToDiscordOutTopic(message, event);
   });
-
-  // Log in to Discord with your client's token
-  console.info("Logging in to Discord…");
-  await client.login(token);
-
-  console.info("Starting keepAlive…");
-  await keepAlive(event, context);
-
-  console.info("Logging out of Discord…");
-  await client.destroy();
-
-  console.info("Exiting Lambda…");
 };
